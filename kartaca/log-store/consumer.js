b8ed8f80f9da6a4fbf23bbe8104ea7d3be4c83a4
Can-Yondem <57828038+Can-Yondem@users.noreply.github.com>
2021-04-04 23:57:54 +0300

    Add files via upload

---
+++ kartaca/log-store/consumer.js
@@ -0,0 +1,52 @@
+const {Kafka} =  require("kafkajs");
+const Logs = require("../models/Logs");
+const mongoose = require('mongoose');
+require('dotenv').config("../.env")
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true },() =>
+ console.log("DB'ye bağlantı başarılı..."));
+ createConsumer()
+
+async function createConsumer(){
+    try {
+        const kafka = new Kafka({
+            clientId:"kafka_log_client",
+            brokers:["localhost:9092"]
+        })
+        const consumer = kafka.consumer({
+            groupId : "log_store_consumer_group"
+        });
+        console.log("Consumer'a bağlanılıyor");
+        await consumer.connect();
+        console.log("Consumer'a bağlantı başarılı...");
+
+        
+        //Consumer Subscribe..
+        await consumer.subscribe({
+            topic : "LogStoreTopic",
+            fromBeginning : true 
+        });
+
+        
+        await consumer.run({
+            eachMessage : async result => {
+                console.log(`Gelen Mesaj ${result.message.value} : Partition : => ${result.partition}`);
+                
+                let jsonlog = JSON.parse(result.message.value);
+
+                
+                const log = new Logs({
+                    method: jsonlog.method,
+                    responsetime: jsonlog.responsetime,
+                    timestamp: jsonlog.timestamp
+                });
+                
+               const savedLog = await log.save();
+                
+            }
+        })
+    } catch (error) {
+        console.log("Bir hata oluştu",error);
+    }
+
+}
